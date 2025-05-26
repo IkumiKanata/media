512fdb204b849d27626e525a49cd1b66f56e3913
KanataIkumi <i.kanata0531@gmail.com>
2025-05-26 21:17:26 +0900

    Media列挙型にdescribeメソッドを追加し、メディアの詳細を表示できるようにしました。main関数内でprint_mediaの代わりにdescribeを呼び出すように変更しました。

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ enum Media {
     AudioBook { title: String },
 }
 
+impl Media {
+    fn describe(&self) {
+        match self {
+            Media::Book { title, author } => println!("Book: {} by {}", title, author),
+            Media::Movie { title, director } => println!("Movie: {} by {}", title, director),
+            Media::AudioBook { title } => println!("AudioBook: {}", title),
+        }
+    }
+}
+
 fn print_media(media: Media) {
     println!("{:?}", media);
 }
@@ -14,5 +24,5 @@ fn main() {
         title: String::from("The Great Gatsby"),
         author: String::from("F. Scott Fitzgerald"),
     };
-    print_media(book);
+    book.describe();
 }
