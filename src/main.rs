5bbbd2a3bd5de42858a9d7c7df63eb91e965779c
KanataIkumi <i.kanata0531@gmail.com>
2025-05-27 21:33:21 +0900

    Media列挙型にPodcastとPlaceholderのバリアントを追加し、describeメソッドでそれらを表示できるようにしました。

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ enum Media {
     Book { title: String, author: String },
     Movie { title: String, director: String },
     AudioBook { title: String },
+    Podcast(u32),
+    Placeholder,
 }
 
 impl Media {
@@ -11,6 +13,8 @@ impl Media {
             Media::Book { title, author } => println!("Book: {} by {}", title, author),
             Media::Movie { title, director } => println!("Movie: {} by {}", title, director),
             Media::AudioBook { title } => println!("AudioBook: {}", title),
+            Media::Podcast(episode) => println!("Podcast: {}", episode),
+            Media::Placeholder => println!("Placeholder"),
         }
     }
 }
